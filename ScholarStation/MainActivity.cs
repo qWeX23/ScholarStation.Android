ab7ee3b92e67c00e84dca1fb4d593756306b7b3a
Ben <bjc9001@gmail.com>
2016-01-23 16:00:06 -0600

    First

---
+++ ScholarStation/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace ScholarStation
+{
+	[Activity (Label = "ScholarStation", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+
+			//
+			//afdsdfsdfwefwefw
+			//wefwef/wefew/ewfwefmfew;m
+			//
+			//
+
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.button1);
+			
+			button.Click += delegate {
+				button.Text = string.Format ("{0} clicks!", count++);
+			};
+		}
+	}
+}
+
+
---
+++ ScholarStation/klnlknlknlkn.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ScholarStation
+{
+	[Activity (Label = "klnlknlknlkn")]			
+	public class klnlknlknlkn : Activity
+	{
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+			//qweqwe/
+			//qwrqwrr
+			//qwrqwr/
+
+			///qwrwqrwijfisfijsdfoisdjiof
+			// Create your application here
+		}
+	}
+}
+
